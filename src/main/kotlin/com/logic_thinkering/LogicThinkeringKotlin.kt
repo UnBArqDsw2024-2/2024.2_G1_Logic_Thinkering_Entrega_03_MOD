c52cbafc2133c2c867dc12c6fd611e8d6525a4b0
Hunter104 <000plagueinc@gmail.com>
2025-01-08 00:39:53 -0300

    conserta blocos sem grupo

--- src/main/kotlin/com/logic_thinkering/LogicThinkeringKotlin.kt
+++ src/main/kotlin/com/logic_thinkering/LogicThinkeringKotlin.kt
@@ -22,6 +22,7 @@ fun initialize() = register {
     }
     blocks {
         settings = Settings.copy(Blocks.REPEATER)
+        itemGroup = MOD_GROUP
         ::ORGate with "or_gate"
         ::ANDGate with "and_gate"
         ::XORGate with "xor_gate"
